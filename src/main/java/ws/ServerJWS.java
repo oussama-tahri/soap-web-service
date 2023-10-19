282decb3c8ae8605848626ca281760c19a3eec04
Oussama <odmf77@gmail.com>
2023-10-19 22:11:46 +0100

    testing soap web services

---
+++ src/main/java/ws/ServerJWS.java
@@ -0,0 +1,10 @@
+package ws;
+
+import jakarta.xml.ws.Endpoint;
+
+public class ServerJWS {
+    public static void main(String[] args) {
+        Endpoint.publish("http://0.0.0.0:5566/",new BanqueService());
+        System.out.println("Web service déployé sur http://localhost:5566/");
+    }
+}
